c0b5fd1577f9cefb3e2bc0456e9c9c29b3badaf4
vivek <vivek.uppal@gmail.com>
2023-07-17 14:53:49 -0400

    Generate binaries using batch file.

--- generate_binary.bat
+++ generate_binary.bat
@@ -8,10 +8,23 @@ SET ZIP_FILE_NAME=transcribe.zip
 REM pyinstaller --clean --noconfirm --specpath C:\\git\\output --distpath C:\\git\\output\dist -n transcribe.exe --log-level DEBUG --recursive-copy-metadata "openai-whisper" main.py
 
 SET PYINSTALLER_DIST_PATH=%OUTPUT_DIR%\dist
+SET PYINSTALLER_TEMP_PATH=%OUTPUT_DIR%\temp
 ECHO %PYINSTALLER_DIST_PATH%
 
-echo pyinstaller --clean --noconfirm --specpath %OUTPUT_DIR% --distpath %PYINSTALLER_DIST_PATH% -n %EXECUTABLE_NAME% --log-level DEBUG --add-data "C:\pyenv\transcribe\Lib\site-packages\whisper\;."  --add-data "C:\pyenv\transcribe\Lib\site-packages\whisper\assets\mel_filters.npz;." main.py
-copy C:\\git\\transcribe\\tiny.en.pt C:\\git\\output\\dist\\transcribe.exe\\tiny.en.pt
-copy C:\\pyenv\\transcribe\\Lib\\site-packages\\whisper\\assets\\mel_filters.npz C:\\git\\output\\dist\\transcribe.exe\\whisper\\assets\\mel_filters.npz
+pyinstaller --clean --noconfirm --workpath %PYINSTALLER_TEMP_PATH% --specpath %OUTPUT_DIR% --distpath %PYINSTALLER_DIST_PATH% -n %EXECUTABLE_NAME% --log-level DEBUG main.py
+
+SET ASSETS_DIR_SRC=%LIBSITE_PACAGES_DIR%\whisper\assets\
+SET ASSETS_DIR_DEST=%PYINSTALLER_DIST_PATH%\%EXECUTABLE_NAME%\whisper\assets
+
+REM ensure the appropriate directories exist
+if not exist %PYINSTALLER_DIST_PATH%\%EXECUTABLE_NAME%\whisper mkdir %PYINSTALLER_DIST_PATH%\%EXECUTABLE_NAME%\whisper
+if not exist %ASSETS_DIR_DEST% mkdir %ASSETS_DIR_DEST%
+
+REM Copy appropriate files to the dir
+copy %SOURCE_DIR%\tiny.en.pt %OUTPUT_DIR%\dist\%EXECUTABLE_NAME%\tiny.en.pt
+copy %ASSETS_DIR_SRC%\mel_filters.npz %ASSETS_DIR_DEST%
+copy %ASSETS_DIR_SRC%\gpt2.tiktoken %ASSETS_DIR_DEST%
 
 REM Code for zipping the final package
+
+REM "D:\Code\transcribe\output\main\whisper\assets\mel_filters.npz"
