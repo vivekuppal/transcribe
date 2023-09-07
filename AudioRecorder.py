49bb0d5cc44d8bba2101dd39f405c333fa838abb
vivek <vivek.uppal@gmail.com>
2023-09-07 16:54:12 -0400

    checkpoint between win, mac

--- AudioRecorder.py
+++ AudioRecorder.py
@@ -41,6 +41,12 @@ driver_type = {
     13: 'Windows Vista Audio stack architecture'
 }
 
+# This needs to be formatted better
+# Attempt to get more info from it like, device_type Mic vs speaker
+def print_detailed_audio_info_2():
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print(f'Audio device with name "{name}" found at index {index}')
+
 
 # This needs to be formatted better
 # Attempt to get more info from it like, device_type Mic vs speaker
