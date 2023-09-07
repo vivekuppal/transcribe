6c447451004af754de8c1b60031624538123ef32
Abhinav Uppal <abuuppal@abus-mac-air.lan>
2023-09-07 17:52:57 -0400

    Checkpoint on mac

--- AudioRecorder.py
+++ AudioRecorder.py
@@ -286,7 +286,7 @@ class SpeakerRecorder(BaseRecorder):
         elif os_name == 'Darwin':
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
                 # print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
-                if name == BLACKHOLE_MIC_NAME:
+                if name == MBP_SPEAKER_NAME:
                     self.device_index = index
 
             p = pyaudio.PyAudio()
