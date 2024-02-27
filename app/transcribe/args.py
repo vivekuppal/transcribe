f1fc7e3f35b385971f9a2f7a9d5509220a729f5a
vivek <vivek.uppal@gmail.com>
2024-02-27 14:21:36 -0500

    clean up

--- app/transcribe/args.py
+++ app/transcribe/args.py
@@ -46,31 +46,7 @@ def create_args() -> argparse.Namespace:
         'tiny', 'base', 'small', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large'],
         default='base',
         help='Specify the OpenAI Local Transcription model file to use.'
-        '\nThe necessary model files will be downloaded once at run time.'
-        '\n The files can also be manually downloaded from these locations.'
-        '\ntiny multi-lingual model has to be downloaded from the link   '
-        'https://drive.google.com/file/d/1M4AFutTmQROaE9xk2jPc5Y4oFRibHhEh/view?usp=drive_link'
-        '\nbase english model has to be downloaded from the link         '
-        'https://openaipublic.azureedge.net/main/whisper/models/25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead/base.en.pt'  # noqa: E501  pylint: disable=C0301
-        '\nbase multi-lingual model has to be downloaded from the link   '
-        'https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt'  # noqa: E501  pylint: disable=C0115
-        '\nsmall english model has to be downloaded from the link        '
-        'https://openaipublic.azureedge.net/main/whisper/models/f953ad0fd29cacd07d5a9eda5624af0f6bcf2258be67c92b79389873d91e0872/small.en.pt'  # noqa: E501  pylint: disable=C0115
-        '\nsmall multi-lingual model has to be downloaded from the link  '
-        'https://openaipublic.azureedge.net/main/whisper/models/9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794/small.pt'  # noqa: E501  pylint: disable=C0115
-        '\n\nThe models below require higher computing power: \n\n'
-        '\nmedium english model has to be downloaded from the link       '
-        'https://openaipublic.azureedge.net/main/whisper/models/d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f/medium.en.pt'  # noqa: E501  pylint: disable=C0115
-        '\nmedium multi-lingual model has to be downloaded from the link '
-        'https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt'  # noqa: E501  pylint: disable=C0115
-        '\nlarge model has to be downloaded from the link                '
-        'https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt'  # noqa: E501  pylint: disable=C0115
-        '\nlarge-v1 model has to be downloaded from the link             '
-        'https://openaipublic.azureedge.net/main/whisper/models/e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a/large-v1.pt'  # noqa: E501  pylint: disable=C0115
-        '\nlarge-v2 model has to be downloaded from the link             '
-        'https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt'  # noqa: E501  pylint: disable=C0115
-        '\nlarge-v3 model has to be downloaded from the link             '
-        'https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt')  # noqa: E501  pylint: disable=C0115
+        '\nThe necessary model files will be downloaded once at run time.')  # noqa: E501  pylint: disable=C0115
     cmd_args.add_argument('-l', '--list_devices', action='store_true',
                           help='List all audio drivers and audio devices on this machine.'
                           '\nUse this list index to select the microphone, speaker device for transcription.')
--- sdk/transcriber_models.py
+++ sdk/transcriber_models.py
@@ -112,32 +112,7 @@ class WhisperSTTModel(STTModelInterface):
             file_url = 'https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/' + self.model_name
             utilities.download_using_bits(file_url=file_url, file_path=self.model_filename)
         else:
-            print(f'Download the transcription model file and add it to the directory: \
-                {os.getcwd() + MODELS_DIR}')
-            print('tiny multi-lingual model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/65147644a518d12f04e32d6f3b26facc3f8dd46e5390956a9424a650c0ce22b9/tiny.pt')  # noqa: E501  pylint: disable=C0115
-            print('tiny multi-lingual model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/d3dd57d32accea0b295c96e26691aa14d8822fac7d9d27d5dc00b4ca2826dd03/tiny.en.pt')  # noqa: E501  pylint: disable=C0115
-            print('base english model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead/base.en.pt')  # noqa: E501  pylint: disable=C0115
-            print('base multi-lingual model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt')  # noqa: E501  pylint: disable=C0115
-            print('small english model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/f953ad0fd29cacd07d5a9eda5624af0f6bcf2258be67c92b79389873d91e0872/small.en.pt')  # noqa: E501  pylint: disable=C0115
-            print('small multi-lingual model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794/small.pt')  # noqa: E501  pylint: disable=C0115
-            print('medium english model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f/medium.en.pt')  # noqa: E501  pylint: disable=C0115
-            print('medium multi-lingual model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt')  # noqa: E501  pylint: disable=C0115
-            print('large model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt')  # noqa: E501  pylint: disable=C0115
-            print('large-v1 model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a/large-v1.pt')  # noqa: E501  pylint: disable=C0115
-            print('large-v2 model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt')  # noqa: E501  pylint: disable=C0115
-            print('large-v3 model has to be downloaded from the link \
-                    https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt')  # noqa: E501  pylint: disable=C0115
+            print('Could not find the correct model file')
             sys.exit()
 
     def get_transcription(self, wav_file_path) -> dict:
