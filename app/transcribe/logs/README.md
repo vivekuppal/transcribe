6149bcb46c9d1690e3a2e346b28e6fbbba8b0a85
Vivek Uppal <vivek.uppal@gmail.com>
2024-05-23 18:05:26 -0400

    First phase of refactor, error handling in UI (#222)
    refactor

--- app/transcribe/logs/README.md
+++
@@ -1,3 +0,0 @@
-All log files for transcribe are produced in this folder.
-
-All files in this folder can be deleted without impacting any functional impact to the application.
--- app/transcribe/ui.py
+++ app/transcribe/ui.py
@@ -30,6 +30,7 @@ class UICallbacks:
     """All callbacks for UI"""
 
     global_vars: TranscriptionGlobals
+    ui_filename: str = None
 
     def __init__(self):
         self.global_vars = TranscriptionGlobals()
@@ -40,7 +41,10 @@ class UICallbacks:
         """
         logger.info(UICallbacks.copy_to_clipboard.__name__)
         self.capture_action("Copy transcript to clipboard")
-        pyperclip.copy(self.global_vars.transcriber.get_transcript())
+        try:
+            pyperclip.copy(self.global_vars.transcriber.get_transcript())
+        except Exception as e:
+            logger.error(f"Error copying to clipboard: {e}")
 
     def save_file(self):
         """Save transcription text data to file.
@@ -51,46 +55,63 @@ class UICallbacks:
                                                     title='Save Transcription',
                                                     filetypes=[("Text Files", "*.txt")])
         self.capture_action(f'Save transcript to file:{filename}')
-        if filename == '':
+        if not filename:
             return
-        with open(file=filename, mode="w", encoding='utf-8') as file_handle:
-            file_handle.write(self.global_vars.transcriber.get_transcript())
+        try:
+            with open(file=filename, mode="w", encoding='utf-8') as file_handle:
+                file_handle.write(self.global_vars.transcriber.get_transcript())
+        except Exception as e:
+            logger.error(f"Error saving file {filename}: {e}")
 
     def freeze_unfreeze(self):
-        """Respond to start / stop of seeking responses from openAI API"""
+        """Respond to start / stop of seeking responses from openAI API
+        """
         logger.info(UICallbacks.freeze_unfreeze.__name__)
-        # Invert the state
-        self.global_vars.responder.enabled = not self.global_vars.responder.enabled
-        self.capture_action(f'{"Enabled " if self.global_vars.responder.enabled else "Disabled "} continuous LLM responses')
-        self.global_vars.freeze_button.configure(
-            text="Suggest Responses Continuously" if not self.global_vars.responder.enabled else "Do Not Suggest Responses Continuously"
+        try:
+            # Invert the state
+            self.global_vars.responder.enabled = not self.global_vars.responder.enabled
+            self.capture_action(f'{"Enabled " if self.global_vars.responder.enabled else "Disabled "} continuous LLM responses')
+            self.global_vars.freeze_button.configure(
+                text="Suggest Responses Continuously" if not self.global_vars.responder.enabled else "Do Not Suggest Responses Continuously"
             )
+        except Exception as e:
+            logger.error(f"Error toggling responder state: {e}")
 
-    # to enable/disable speaker/microphone when args are given or button is pressed
     def enable_disable_speaker(self, editmenu):
-        """Toggles the state of speaker"""
-        self.global_vars.speaker_audio_recorder.enabled = not self.global_vars.speaker_audio_recorder.enabled
-        editmenu.entryconfigure(2, label="Disable Speaker" if self.global_vars.speaker_audio_recorder.enabled else "Enable Speaker")
-        self.capture_action(f'{"Enabled " if self.global_vars.speaker_audio_recorder.enabled else "Disabled "} speaker input')
+        """Toggles the state of speaker
+        """
+        try:
+            self.global_vars.speaker_audio_recorder.enabled = not self.global_vars.speaker_audio_recorder.enabled
+            editmenu.entryconfigure(2, label="Disable Speaker" if self.global_vars.speaker_audio_recorder.enabled else "Enable Speaker")
+            self.capture_action(f'{"Enabled " if self.global_vars.speaker_audio_recorder.enabled else "Disabled "} speaker input')
+        except Exception as e:
+            logger.error(f"Error toggling speaker state: {e}")
 
     def enable_disable_microphone(self, editmenu):
-        """Toggles the state of microphone"""
-        self.global_vars.user_audio_recorder.enabled = not self.global_vars.user_audio_recorder.enabled
-        editmenu.entryconfigure(3, label="Disable Microphone" if self.global_vars.user_audio_recorder.enabled else "Enable Microphone")
-        self.capture_action(f'{"Enabled " if self.global_vars.user_audio_recorder.enabled else "Disabled "} microphone input')
+        """Toggles the state of microphone
+        """
+        try:
+            self.global_vars.user_audio_recorder.enabled = not self.global_vars.user_audio_recorder.enabled
+            editmenu.entryconfigure(3, label="Disable Microphone" if self.global_vars.user_audio_recorder.enabled else "Enable Microphone")
+            self.capture_action(f'{"Enabled " if self.global_vars.user_audio_recorder.enabled else "Disabled "} microphone input')
+        except Exception as e:
+            logger.error(f"Error toggling microphone state: {e}")
 
     def update_interval_slider_value(self, slider_value):
         """Update interval slider label to match the slider value
            Update the config value
         """
-        config_obj = configuration.Config()
-        # Save config
-        altered_config: dict = {'General': {'llm_response_interval': int(slider_value)}}
-        config_obj.add_override_value(altered_config)
+        try:
+            config_obj = configuration.Config()
+            # Save config
+            altered_config = {'General': {'llm_response_interval': int(slider_value)}}
+            config_obj.add_override_value(altered_config)
 
-        label_text = f'LLM Response interval: {int(slider_value)} seconds'
-        self.global_vars.update_interval_slider_label.configure(text=label_text)
-        self.capture_action(f'Update LLM response interval to {int(slider_value)}')
+            label_text = f'LLM Response interval: {int(slider_value)} seconds'
+            self.global_vars.update_interval_slider_label.configure(text=label_text)
+            self.capture_action(f'Update LLM response interval to {int(slider_value)}')
+        except Exception as e:
+            logger.error(f"Error updating slider value: {e}")
 
     def get_response_now(self):
         """Get response from LLM right away
@@ -108,21 +129,33 @@ class UICallbacks:
         response_ui_thread.daemon = True
         response_ui_thread.start()
 
-    def get_response_now_threaded(self):
-        """Update response ui in a separate thread
+    def get_response_selected_now_threaded(self, text: str):
+        """Update response UI in a separate thread
         """
-        self.global_vars.update_response_now = True
-        response_string = self.global_vars.responder.generate_response_from_transcript_no_check()
-        self.global_vars.update_response_now = False
-        # Set event to play the recording audio if required
-        if self.global_vars.read_response:
-            self.global_vars.audio_player_var.speech_text_available.set()
-        self.global_vars.response_textbox.configure(state="normal")
-        if response_string is not None and response_string != '':
-            write_in_textbox(self.global_vars.response_textbox, response_string)
-        write_in_textbox(self.global_vars.response_textbox, response_string)
-        self.global_vars.response_textbox.configure(state="disabled")
-        self.global_vars.response_textbox.see("end")
+        self.update_response_ui_threaded(lambda: self.global_vars.responder.generate_response_for_selected_text(text))
+
+    def get_response_now_threaded(self):
+        """Update response UI in a separate thread
+        """
+        self.update_response_ui_threaded(self.global_vars.responder.generate_response_from_transcript_no_check)
+
+    def update_response_ui_threaded(self, response_generator):
+        """Helper method to update response UI in a separate thread
+        """
+        try:
+            self.global_vars.update_response_now = True
+            response_string = response_generator()
+            self.global_vars.update_response_now = False
+            # Set event to play the recording audio if required
+            if self.global_vars.read_response:
+                self.global_vars.audio_player_var.speech_text_available.set()
+            self.global_vars.response_textbox.configure(state="normal")
+            if response_string:
+                write_in_textbox(self.global_vars.response_textbox, response_string)
+            self.global_vars.response_textbox.configure(state="disabled")
+            self.global_vars.response_textbox.see("end")
+        except Exception as e:
+            logger.error(f"Error in threaded response: {e}")
 
     def get_response_selected_now(self):
         """Get response from LLM right away for selected_text
@@ -142,48 +175,34 @@ class UICallbacks:
         response_ui_thread.daemon = True
         response_ui_thread.start()
 
-    def get_response_selected_now_threaded(self, text: str):
-        """Update response ui in a separate thread
-        """
-        self.global_vars.update_response_now = True
-        response_string = self.global_vars.responder.generate_response_for_selected_text(text=text)
-        self.global_vars.update_response_now = False
-        # Set event to play the recording audio if required
-        if self.global_vars.read_response:
-            self.global_vars.audio_player_var.speech_text_available.set()
-        self.global_vars.response_textbox.configure(state="normal")
-        if response_string is not None and response_string != '':
-            write_in_textbox(self.global_vars.response_textbox, response_string)
-        write_in_textbox(self.global_vars.response_textbox, response_string)
-        self.global_vars.response_textbox.configure(state="disabled")
-        self.global_vars.response_textbox.see("end")
-
     def summarize_threaded(self):
-        """Get summary from LLM in a separate thread
-        """
+        """Get summary from LLM in a separate thread"""
         global pop_up  # pylint: disable=W0603
-        print('Summarizing...')
-        popup_msg_no_close(title='Summary', msg='Creating a summary')
-        summary = self.global_vars.responder.summarize()
-        # When API key is not specified, give a chance for the thread to initilize
+        try:
+            print('Summarizing...')
+            popup_msg_no_close(title='Summary', msg='Creating a summary')
+            summary = self.global_vars.responder.summarize()
+            # When API key is not specified, give a chance for the thread to initialize
 
-        if pop_up is not None:
-            try:
-                pop_up.destroy()
-            except Exception as e:
-                # Somehow we get the exception
-                # RuntimeError: main thread is not in main loop
-                logger.info('Exception in summarize_threaded')
-                logger.info(e)
+            if pop_up is not None:
+                try:
+                    pop_up.destroy()
+                except Exception as e:
+                    # Somehow we get the exception
+                    # RuntimeError: main thread is not in main loop
+                    logger.info('Exception in summarize_threaded')
+                    logger.info(e)
 
-            pop_up = None
-        if summary is None:
-            popup_msg_close_button(title='Summary',
-                                   msg='Failed to get summary. Please check you have a valid API key.')
-            return
+                pop_up = None
+            if summary is None:
+                popup_msg_close_button(title='Summary',
+                                       msg='Failed to get summary. Please check you have a valid API key.')
+                return
 
-        # Enhancement here would be to get a streaming summary
-        popup_msg_close_button(title='Summary', msg=summary)
+            # Enhancement here would be to get a streaming summary
+            popup_msg_close_button(title='Summary', msg=summary)
+        except Exception as e:
+            logger.error(f"Error in summarize_threaded: {e}")
 
     def summarize(self):
         """Get summary response from LLM
@@ -205,69 +224,86 @@ class UICallbacks:
 
     def set_transcript_state(self):
         """Enables, disables transcription.
-           Text of menu item File -> Pause Transcription toggles accordingly"""
+           Text of menu item File -> Pause Transcription toggles accordingly
+        """
         logger.info(UICallbacks.set_transcript_state.__name__)
-        self.global_vars.transcriber.transcribe = not self.global_vars.transcriber.transcribe
-        self.capture_action(f'{"Enabled " if self.global_vars.transcriber.transcribe else "Disabled "} transcription.')
-        if self.global_vars.transcriber.transcribe:
-            self.global_vars.filemenu.entryconfigure(1, label="Pause Transcription")
-        else:
-            self.global_vars.filemenu.entryconfigure(1, label="Start Transcription")
+        try:
+            self.global_vars.transcriber.transcribe = not self.global_vars.transcriber.transcribe
+            self.capture_action(f'{"Enabled " if self.global_vars.transcriber.transcribe else "Disabled "} transcription.')
+            if self.global_vars.transcriber.transcribe:
+                self.global_vars.filemenu.entryconfigure(1, label="Pause Transcription")
+            else:
+                self.global_vars.filemenu.entryconfigure(1, label="Start Transcription")
+        except Exception as e:
+            logger.error(f"Error setting transcript state: {e}")
 
     def open_link(self, url: str):
-        """Open the link in a web browser"""
+        """Open the link in a web browser
+        """
         self.capture_action(f'Navigate to {url}.')
-        webbrowser.open(url=url, new=2)
+        try:
+            webbrowser.open(url=url, new=2)
+        except Exception as e:
+            logger.error(f"Error opening URL {url}: {e}")
 
     def open_github(self):
         """Link to git repo main page
         """
         self.capture_action('open_github.')
-        webbrowser.open(url='https://github.com/vivekuppal/transcribe?referer=desktop', new=2)
+        self.open_link('https://github.com/vivekuppal/transcribe?referer=desktop')
 
     def open_support(self):
         """Link to git repo issues page
         """
         self.capture_action('open_support.')
-        webbrowser.open(url='https://github.com/vivekuppal/transcribe/issues/new?referer=desktop',
-                        new=2)
+        self.open_link('https://github.com/vivekuppal/transcribe/issues/new?referer=desktop')
 
     def capture_action(self, action_text: str):
-        """write to file"""
-        data_dir = utilities.get_data_path(app_name='Transcribe')
-        filename = utilities.incrementing_filename(filename=f'{data_dir}/logs/ui',
-                                                   extension='txt')
-        with open(filename, mode='a', encoding='utf-8') as ui_file:
-            ui_file.write(f'{datetime.datetime.now()}: {action_text}\n')
+        """Write to file
+        """
+        try:
+            if not self.ui_filename:
+                data_dir = utilities.get_data_path(app_name='Transcribe')
+                self.ui_filename = utilities.incrementing_filename(filename=f'{data_dir}/logs/ui', extension='txt')
+            with open(self.ui_filename, mode='a', encoding='utf-8') as ui_file:
+                ui_file.write(f'{datetime.datetime.now()}: {action_text}\n')
+        except Exception as e:
+            logger.error(f"Error capturing action {action_text}: {e}")
 
     def set_audio_language(self, lang: str):
         """Alter audio language in memory and persist it in config file
         """
-        self.global_vars.transcriber.stt_model.set_lang(lang)
-        config_obj = configuration.Config()
-        # Save config
-        altered_config: dict = {'OpenAI': {'audio_lang': lang}}
-        config_obj.add_override_value(altered_config)
+        try:
+            self.global_vars.transcriber.stt_model.set_lang(lang)
+            config_obj = configuration.Config()
+            # Save config
+            altered_config = {'OpenAI': {'audio_lang': lang}}
+            config_obj.add_override_value(altered_config)
+        except Exception as e:
+            logger.error(f"Error setting audio language: {e}")
 
     def set_response_language(self, lang: str):
         """Alter response language in memory and persist it in config file
         """
-        config_obj = configuration.Config()
-        altered_config: dict = {'OpenAI': {'response_lang': lang}}
-        # Save config
-        config_obj.add_override_value(altered_config)
-        config_data = config_obj.data
+        try:
+            config_obj = configuration.Config()
+            altered_config = {'OpenAI': {'response_lang': lang}}
+            # Save config
+            config_obj.add_override_value(altered_config)
+            config_data = config_obj.data
 
-        # Create a new system prompt
-        prompt = config_data["General"]["system_prompt"]
-        response_lang = config_data["OpenAI"]["response_lang"]
-        if response_lang is not None:
-            prompt += f'.  Respond exclusively in {response_lang}.'
-        convo = self.global_vars.convo
-        convo.update_conversation(persona=constants.PERSONA_SYSTEM,
-                                  text=prompt,
-                                  time_spoken=datetime.datetime.utcnow(),
-                                  update_previous=True)
+            # Create a new system prompt
+            prompt = config_data["General"]["system_prompt"]
+            response_lang = config_data["OpenAI"]["response_lang"]
+            if response_lang is not None:
+                prompt += f'.  Respond exclusively in {response_lang}.'
+            convo = self.global_vars.convo
+            convo.update_conversation(persona=constants.PERSONA_SYSTEM,
+                                      text=prompt,
+                                      time_spoken=datetime.datetime.utcnow(),
+                                      update_previous=True)
+        except Exception as e:
+            logger.error(f"Error setting response language: {e}")
 
 
 def popup_msg_no_close_threaded(title, msg):
